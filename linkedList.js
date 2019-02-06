35f02a605792f9e0a09cc4825aeedd7d42da40da
Muhammed Rashid <muhammed.rashid@ibm.com>
2019-02-05 16:57:28 -0800

    add addToHead method

--- linkedList.js
+++ linkedList.js
@@ -8,3 +8,14 @@ function Node(value, next, prev) {
   this.next = next;
   this.prev = prev;
 }
+
+LinkedList.prototype.addToHead = function (value) {
+  const newHead = new Node(value, this.head, null);
+  if (this.head === null) {
+    this.head = newHead;
+    this.tail = newHead;
+  } else {
+    this.head.prev = newHead;
+    this.head = newHead;
+  }
+}
