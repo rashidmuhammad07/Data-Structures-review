cfe4ca3ceafd97281fc648c256413498a1734286
Muhammed Rashid <muhammed.rashid@ibm.com>
2019-02-08 11:11:31 -0800

    add set method

--- HashTable.js
+++ HashTable.js
@@ -16,3 +16,21 @@ HashTable.prototype.print = function() {
   console.log(this.storage);
 }
 
+HashTable.prototype.set = function(key, value) {
+  let index = hash(key, this.storageLimit);
+  if (this.storage[index] === undefined) {
+    this.storage[index] = [ [key, value] ];
+  } else {
+    let inserted = false;
+    for (let i = 0; i < this.storage[index].length; i++) {
+      if (this.storage[index][i][0] === key) {
+        this.storage[index][i][1] = value;
+        inserted = true;
+      }
+    }
+
+    if (inserted = false) {
+      this.strorage[index].push([key, value]);
+    }
+  }
+}
