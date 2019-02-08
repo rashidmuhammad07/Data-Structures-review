f7c876669fb9e9a3d702fc37800a9e9cd4ed1670
Muhammed Rashid <muhammed.rashid@ibm.com>
2019-02-08 15:15:26 -0800

    add pop method

--- Stack.js
+++ Stack.js
@@ -8,3 +8,14 @@ Stack.prototype.push = function(value) {
   this.storage[this.count] = value;
 }
 
+Stack.prototype.pop = function() {
+  if (this.count === 0) {
+    return undefined;
+  }
+  let value = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+
+  return value;
+}
+
