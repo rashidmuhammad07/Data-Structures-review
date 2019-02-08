0dc4c4b3be16f97111164b36e780f42d204da716
Muhammed Rashid <muhammed.rashid@ibm.com>
2019-02-08 15:42:43 -0800

    add first, last and size methods

--- Queue.js
+++ Queue.js
@@ -9,3 +9,21 @@ Queue.prototype.enqueue = function (value) {
 Queue.prototype.dequeue = function (value) {
   this.storage.pop();
 }
+
+Queue.prototype.first = function (value) {
+  if (this.storage.length < 1) {
+    return undefined;
+  }
+  return this.storage[0];
+}
+
+Queue.prototype.last = function (value) {
+  if (this.storage.length < 1) {
+    return undefined;
+  }
+  return this.storage[this.storage.length - 1];
+}
+
+Queue.prototype.size = function (value) {
+  return this.storage.length;
+}
