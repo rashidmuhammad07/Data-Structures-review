efe87294dfbe8c45a247c5577ffaa82dec088d88
Muhammed Rashid <muhammed.rashid@ibm.com>
2019-02-20 12:47:11 -0800

    initial implementation of binary search tree

---
+++ BinarySearchTree.js
@@ -0,0 +1,66 @@
+let BTree = function () {
+  this.root = null;
+}
+
+let Node = function (val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+BTree.prototype.addValue = function(value) {
+  let newNode = new Node(value);
+  if (this.root === null) {
+    this.root = newNode;
+  } else {
+    this.root.addChildren(newNode);
+  }
+}
+
+Node.prototype.addChildren = function(newNode) {
+  if (newNode.value < this.value) {
+    if (this.left !== null) {
+      this.left.addChildren(newNode);
+    } else {
+      this.left = newNode;
+    }
+  } else if (newNode.value > this.value) {
+    if (this.right !== null) {
+      this.right.addChildren(newNode);
+    } else {
+      this.right = newNode;
+    }
+  }
+}
+
+BTree.prototype.traverse = function() {
+  this.root.visit();
+}
+
+BTree.prototype.search = function(value) {
+  this.root.find(value);
+} 
+
+Node.prototype.visit = function() {
+  if (this.left !== null) {
+    this.left.visit();
+  }
+
+  console.log(this.value);
+  
+  if (this.right !== null) {
+     this.right.visit();
+  }
+}
+
+Node.prototype.find = function (value) {
+  if (this.value === value) {
+    console.log(`FOUND: ${value}`);
+  } else if (value > this.value && this.right !== null) {
+    this.right.find(value);
+  } else if (value < this.value && this.left !== null) {
+    this.left.find(value);
+  } else {
+    console.log('NOT FOUND');
+  }
+}
